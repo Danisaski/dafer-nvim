5459cce742336a7cd5c511c4cc9028c9924c8a17
Daniel <dafernandezperez@gmail.com>
2025-01-04 13:03:46 +0100

    Tmux implementation

--- lua/daniel/statusbar.lua
+++ lua/daniel/statusbar.lua
@@ -1,17 +1,14 @@
 require("lualine").setup({
-	options = {
-		theme = "catppuccin", -- Choose a theme that fits your style
-		icons_enabled = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics", "lsp_progress" }, -- Add LSP progress
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress", "location" },
-		lualine_z = {
-			function()
-				return os.date("%H:%M:%S") -- Display current time in HH:MM:SS format
-			end },
-	},
+    options = {
+        theme = "catppuccin", -- Choose a theme that fits your style
+        icons_enabled = true,
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics", "lsp_progress" }, -- Add LSP progress
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+    }
 })

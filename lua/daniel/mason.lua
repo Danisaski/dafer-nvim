557d79a6ddd4e5b62452663f97af9e90dfa6aea2
Daniel <dafernandezperez@gmail.com>
2025-01-08 22:20:41 +0100

    Added LSPs

--- lua/daniel/mason.lua
+++ lua/daniel/mason.lua
@@ -1,15 +1,30 @@
 -- ~/.config/nvim/lua/daniel/mason.lua
 require("mason").setup()
+
 require("mason-lspconfig").setup({
-	ensure_installed = { "pyright", "ruff", 'lua_ls', 'rust_analyzer' }, -- Add other LSP servers as needed
-	handlers = { function(server_name)
-		require('lspconfig')[server_name].setup({})
-	end }
+    ensure_installed = {
+        "pyright", "ruff", -- Python
+        "lua_ls",          -- Lua
+        "rust_analyzer",   -- Rust
+        "clangd",          -- C/C++
+        -- "gopls",           -- Go
+        "zls",             -- Zig
+        "ltex",            -- English grammar, LaTeX, and Markdown
+        "marksman"         -- Markdown
+    },
+    handlers = {
+        function(server_name)
+            -- Default configuration
+            require('lspconfig')[server_name].setup({})
+        end
+    }
 })
 
 -- Setup for other tools
 require("mason-tool-installer").setup({
-	ensure_installed = { "debugpy" }, -- Add other non-LSP tools as needed
-	auto_update = true,        -- Automatically update installed tools
-	run_on_start = true        -- Run the installer on startup
+    ensure_installed = {
+        "debugpy",      -- Existing tools
+    },
+    auto_update = true, -- Automatically update installed tools
+    run_on_start = true -- Run the installer on startup
 })

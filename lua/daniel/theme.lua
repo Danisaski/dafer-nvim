0cbe1b35dd22b3d391c254c06704ccef668ce4ab
DanielSener <dafernandezperez@gmail.com>
2025-01-24 09:17:23 +0100

    Removed EOB showing.

--- lua/daniel/theme.lua
+++ lua/daniel/theme.lua
@@ -9,6 +9,7 @@ function M.setup()
     vim.g.gruvbox_material_enable_bold = true
     vim.g.gruvbox_material_ui_contrast = 'high'
     vim.g.gruvbox_material_inlay_hints_background = 'dimmed'
+    vim.g.gruvbox_material_show_eob = false
     -- vim.g.gruvbox_material_float_style='dim'
     -- vim.g.gruvbox_material_transparent_background=2
     -- vim.g.gruvbox_material_dim_inactive_windows = true

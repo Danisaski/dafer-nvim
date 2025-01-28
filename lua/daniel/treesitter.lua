ee2d9535e3af0219f0ed9a6433453822b5f1637e
dafer-wsl-sener <dafernandezperez@gmail.com>
2025-01-28 13:32:56 +0100

    Added java installation and treesitter languages

--- lua/daniel/treesitter.lua
+++ lua/daniel/treesitter.lua
@@ -1,17 +1,17 @@
 ---@diagnostic disable: missing-fields, undefined-global
 require('nvim-treesitter.configs').setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "rust", "json", "zig", "regex"},
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "rust", "json", "zig", "regex", "gitignore", "toml" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    additional_vim_regex_highlighting = false,
-  },
+        additional_vim_regex_highlighting = true,
+    },
 }

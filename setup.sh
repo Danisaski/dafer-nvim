19a8773cbc5ee7e4259d03ada03f895456934f34
Daniel <88162416+Danisaski@users.noreply.github.com>
2025-01-28 12:47:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ if ask "Install the complete developer environment?"; then
     elif [ "$OS_TYPE" = "debian" ]; then
         print_message "Updating system and installing essential packages..."
         sudo apt update && sudo apt upgrade -y
-        sudo apt install -y build-essential git curl wget unzip zip sudo zsh tmux fzf ripgrep openssh pkg-config
+        sudo apt install -y build-essential git curl wget unzip zip sudo zsh tmux fzf ripgrep pkg-config
         curl -sS https://starship.rs/install.sh | sh
         LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\\K[^"]*')
         curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"

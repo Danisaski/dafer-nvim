174b55d3a7e62db671c133d1849d101bf8a5bec5
dafer-wsl-sener <dafernandezperez@gmail.com>
2025-02-24 10:23:27 +0100

    Multicursor config

--- lua/daniel/multicursor.lua
+++ lua/daniel/multicursor.lua
@@ -9,36 +9,36 @@ M.setup = function()
     local set = vim.keymap.set
 
     -- Add or skip cursor above/below the main cursor.
-    set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
-    set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
-    set({"n", "x"}, "<C-up>", function() mc.lineSkipCursor(-1) end)
-    set({"n", "x"}, "<C-down>", function() mc.lineSkipCursor(1) end)
+    set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end)
+    set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end)
+    set({ "n", "x" }, "<C-up>", function() mc.lineSkipCursor(-1) end)
+    set({ "n", "x" }, "<C-down>", function() mc.lineSkipCursor(1) end)
 
     -- Add or skip adding a new cursor by matching word/selection
-    set({"n", "x"}, "<C-n>", function() mc.matchAddCursor(1) end)
-    set({"n", "x"}, "<C-s>", function() mc.matchSkipCursor(1) end)
-    set({"n", "x"}, "<C-N>", function() mc.matchAddCursor(-1) end)
-    set({"n", "x"}, "<C-S>", function() mc.matchSkipCursor(-1) end)
+    set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
+    set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
+    set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
+    set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
 
     -- Add all matches in the document
-    set({"n", "x"}, "<leader>A", mc.matchAllAddCursors)
+    set({ "n", "x" }, "<leader>A", mc.matchAllAddCursors)
 
     -- Rotate the main cursor.
-    set({"n", "x"}, "<left>", mc.nextCursor)
-    set({"n", "x"}, "<right>", mc.prevCursor)
+    set({ "n", "x" }, "<left>", mc.nextCursor)
+    set({ "n", "x" }, "<right>", mc.prevCursor)
 
     -- Delete the main cursor.
-    set({"n", "x"}, "<leader>x", mc.deleteCursor, {desc='Delete current cursor'})
+    set({ "n", "x" }, "<leader>x", mc.deleteCursor, { desc = 'Delete current cursor' })
 
     -- Add and remove cursors with control + left click.
     set("n", "<c-leftmouse>", mc.handleMouse)
     set("n", "<c-leftdrag>", mc.handleMouseDrag)
 
     -- Restore cursors if accidentally cleared
-    set("n", "<leader>mc", mc.restoreCursors, {desc='Restore cursors'})
+    set("n", "<leader>mc", mc.restoreCursors, { desc = 'Restore cursors' })
 
     -- Align cursor columns.
-    set("n", "<leader>a", mc.alignCursors, {desc='Aling selected cursors'})
+    set("n", "<leader>a", mc.alignCursors, { desc = 'Aling selected cursors' })
 
     -- Append/insert for each line of visual selections.
     set("x", "I", mc.insertVisual)
@@ -56,4 +56,3 @@ M.setup = function()
 end
 
 return M
-
